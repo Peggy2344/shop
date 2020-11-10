fbb928744180be9bb667d2c326ed5e0bd9498336
Peggy2344 <peggy102104@gmail.com>
2020-11-10 09:37:58 +0800

    整理預設檔案

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 必要引用
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// 根據 icon 的種類引用
+import { faShoppingCart, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons'
+library.add(faShoppingCart, faTrash, faCommentDots)
+
+// 註冊元件
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,10 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: '購物趣'
+    }
   },
   {
     path: '/about',
@@ -16,12 +19,27 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: '網站介紹'
+    }
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import(/* webpackChunkName: "cart" */ '../views/Cart.vue'),
+    meta: {
+      title: '購物車'
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.afterEach((to, from) => {
+  document.title = to.meta.title
 })
 
 export default router
