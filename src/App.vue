f0ce1f779f9af1b7c235bc5610d24034afd4b63e
Peggy2344 <peggy102104@gmail.com>
2020-11-10 10:51:42 +0800

    完成商品頁面

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template lang="pug">
 #app
-  #nav
-    router-link(to='/') Home
-    |  |
-    router-link(to='/about') About
-  router-view
+  b-container
+    b-row
+      b-col.text-center(cols="12")
+        #nav
+          router-link(to='/') Home
+          |  |
+          router-link(to='/about') About
+      b-col(cols="12")
+        router-view
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,42 @@
 <template lang="pug">
   #home
+    b-row
+      b-col(cols="12" md="6" lg="3" v-for="(item, index) in items" :key="index")
+        b-card(:title="item.name" :img-src="item.img" img-top :sub-title="'$'+item.price")
+          b-button(variant="info")
+            font-awesome-icon(:icon="['fas', 'shopping-cart']")
+            | &nbsp; 加入購物車
 </template>
 
 <script>
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      items: [
+        {
+          img: 'https://e.ecimg.tw/items/DYAJCHA900AP7EG/000002_1604373280.jpg',
+          name: 'Airpods Pro',
+          price: 6090
+        },
+        {
+          img: 'https://f.ecimg.tw/items/DYAN70A900AUPA5/000002_1599102543.jpg',
+          name: '小米手環5',
+          price: 845
+        },
+        {
+          img: 'https://c.ecimg.tw/items/DCAN8JA9009OS4V/000002_1596181202.jpg',
+          name: '羅技M720滑鼠',
+          price: 888
+        },
+        {
+          img: 'https://b.ecimg.tw/items/DMBF05A900AWDE6/000002_1601278839.jpg',
+          name: 'Dyson V8 無限吸塵器',
+          price: 12900
+        }
+      ]
+    }
+  }
 }
 </script>
